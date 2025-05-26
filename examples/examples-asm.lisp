e8b9cc26b00f0cf21e3f1a3756c4b60545b595b4
Mattias Brändström <thebrasse@brasse.org>
2025-05-26 14:46:06 +0200

    Add recursive example calculating Fibonacci numbers

--- examples/examples-asm.lisp
+++ examples/examples-asm.lisp
@@ -17,7 +17,7 @@
     (label done)
     (ret acc)))
 
-(defparameter +fibonacci-asm+
+(defparameter +fibonacci-asm-iter+
   #((const n 6)
     (call fibonacci n)
     (get-ret result 0)
@@ -36,3 +36,32 @@
     (jmp loop)
     (label done)
     (ret a)))
+
+(defparameter +fibonacci-asm-recur+
+  #((const n 6)
+    (call fibonacci n)
+    (get-ret result 0)
+    (print result)
+    (halt)
+
+    (label fibonacci)
+    (get-arg n 0)
+
+    (gt tst n 2)                ; return 1 if n <= 2
+    (jnz recur tst)
+    (ret 1)
+
+    (label recur)
+
+    (mov a n)                   ; get fib(n - 1)
+    (sub a a 1)
+    (call fibonacci a)
+    (get-ret fib-1 0)
+
+    (mov b n)                   ; get fib(n - 2)
+    (sub b b 2)
+    (call fibonacci b)
+    (get-ret fib-2 0)
+
+    (add result fib-1 fib-2)    ; fib(n) = fib(n - 1) + fib(n - 2)
+    (ret result)))
